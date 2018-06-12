fa3f6c796739aa60fbd835325a12a3d26f7126df
Melanie MacDonald <melmacd94@gmail.com>
2018-06-12 17:31:54 -0400

    Instructor solution

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,19 @@
 class Instructor
+attr_accessor :name
 
+@@all= []
+
+def initialize(name)
+@name= name
+@@all << self
+end
+
+def self.all
+  @@all
+end
+
+def fail_student(student_first_name_input, test_name)
+  chosen_test = BoatingTest.all.find{|test| test.student.first_name == student_first_name_input && test.name == test_name}
+  chosen_test.status= "failed"
+end
 end
