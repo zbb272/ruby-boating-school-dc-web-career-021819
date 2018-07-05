effad9e7caa4b95a776172adabf412b78817ca11
Melanie MacDonald <melmacd94@gmail.com>
2018-07-05 13:41:39 -0400

    less methods

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,32 +1,24 @@
 class Student
+
 attr_accessor :first_name, :last_name
 
 @@all= []
 
-def initialize(first_name, last_name)
-  @first_name = first_name
-  @last_name = last_name
-  @@all << self
+def initialize(first_name)
+@first_name = first_name
+@@all << self
 end
 
 def self.all
   @@all
 end
 
-def full_name
-  "#{first_name} #{last_name}"
-end
-
-def self.full_names
-  self.all.map{|name| name.full_name}
-end
-
 def add_boating_test(name, status, instructor)
   BoatingTest.new(self, name, status, instructor)
 end
 
-def self.find_student(student_input)
-  self.all.find{|student| student.full_name == student_input}
+def self.find_student(student_first_name)
+  self.all.find{|student| student.first_name == student_first_name}
 end
 
 end
