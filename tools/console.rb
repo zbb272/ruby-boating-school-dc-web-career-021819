e742b7ba0830c8e3584364e3e95d6b34d6867c6f
Melanie MacDonald <melmacd94@gmail.com>
2018-07-09 13:07:18 -0400

    update environment
    Adapted environment to new code challenge environment

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,10 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+# Insert code here to run before hitting the binding.pry
+# This is a convenient place to define variables and/or set up new object instances,
+# so they will be available to test and play around with in your console
+
+binding.pry
+0 #leave this here to ensure binding.pry isn't the last line
+
