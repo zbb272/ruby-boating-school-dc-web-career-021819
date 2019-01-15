aba59fc7a1d18944854b4bcff408c5cd5ca99ccc
Melanie MacDonald <melmacd94@gmail.com>
2019-01-15 16:35:46 -0500

    if test does not exist it creates it

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,14 +13,25 @@ attr_accessor :name
     @@all
   end
 
-  def pass_student(student_first_name, test_name)
+   def pass_student(student_first_name, test_name)
     testtopass= BoatingTest.all.find{|test| test.student.first_name == student_first_name && test.name == test_name}
-    testtopass.status= "passed"
+    puts testtopass
+    if testtopass
+      testtopass.status= "passed"
+    else
+      student= Student.all.find{|student| student.first_name == student_first_name}
+      BoatingTest.new(student, test_name, "passed", self)
+    end
   end
 
-  def fail_student(student_first_name, test_name)
+def fail_student(student_first_name, test_name)
     testtofail= BoatingTest.all.find{|test| test.student.first_name == student_first_name && test.name == test_name}
-    testtofail.status= "failed"
+    if testtofail
+      testtofail.status= "failed"
+    else
+      student= Student.all.find{|student| student.first_name == student_first_name}
+      BoatingTest.new(student, test_name, "failed", self)
+    end
   end
 
 end
