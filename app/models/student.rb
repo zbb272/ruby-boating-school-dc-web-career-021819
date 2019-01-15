98ef3e43e9bd39399f19b935100ab8d594c31f53
Melanie MacDonald <melmacd94@gmail.com>
2019-01-15 13:19:28 -0500

    update to match new instructions grade percentage

--- app/models/student.rb
+++ app/models/student.rb
@@ -20,5 +20,13 @@ end
 def self.find_student(student_first_name)
   self.all.find{|student| student.first_name == student_first_name}
 end
+  
+def grade_percentage
+  studentTests= BoatingTest.all.find_all{|test| test.student.first_name == self.first_name}
+  totalTests= studentTests.length.to_f
+  totalPassed= studentTests.find_all{|test| test.status == "passed"}
+  numPassed= totalPassed.length.to_f
+  percent= (numPassed/totalTests)*100
+end
 
 end
