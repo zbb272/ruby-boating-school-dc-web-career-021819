efa574be5f44fe17ee22e52b8c27c1b74b149cd6
Melanie MacDonald <melmacd94@gmail.com>
2019-01-15 13:21:35 -0500

    make instance methods to match new instruction

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,22 +13,15 @@ attr_accessor :name
     @@all
   end
 
-  def self.pass_student(student_first_name, test_name)
+  def pass_student(student_first_name, test_name)
     testtopass= BoatingTest.all.find{|test| test.student.first_name == student_first_name && test.name == test_name}
     testtopass.status= "passed"
   end
 
-  def self.fail_student(student_first_name, test_name)
+  def fail_student(student_first_name, test_name)
     testtofail= BoatingTest.all.find{|test| test.student.first_name == student_first_name && test.name == test_name}
     testtofail.status= "failed"
   end
 
-  def self.student_grade_percentage(student_first_name)
-    studentTests= BoatingTest.all.find_all{|test| test.student.first_name == student_first_name}
-    totalTests= studentTests.length.to_f
-    totalPassed= studentTests.find_all{|test| test.status == "passed"}
-    numPassed= totalPassed.length.to_f
-    percent= (numPassed/totalTests)*100
-  end
 end
 
